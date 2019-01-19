9050dec06b38dc8a283ba7b83de1d699f491fd13
Gilbert <gilbrater@rocketmail.com>
2019-01-19 14:54:12 +0100

    Project initialization

---
+++ src/main/kotlin/com/weydem/statisticskotlin/StatisticsKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.weydem.statisticskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatisticsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<StatisticsKotlinApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/weydem/statisticskotlin/StatisticsKotlinApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.weydem.statisticskotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class StatisticsKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
